034f7fabc7e03ca9c8df5fce77f3b23ac7ce9825
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-08-25 17:18:31 +0300

    only followed the vid

---
+++ Proj-08-CupcakeCorner/AddressView.swift
@@ -0,0 +1,40 @@
+//
+//  AddressView.swift
+//  Proj-08-CupcakeCorner
+//
+//  Created by Андрей Бородкин on 25.08.2023.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                    Text("Checkout")
+                }
+                
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}

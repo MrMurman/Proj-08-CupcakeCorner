5e5e7ab4f41fa99c70353fad5cc238f8bd4d79bb
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-08-25 14:12:14 +0300

    Initial Commit

---
+++ Proj-08-CupcakeCorner/Proj_08_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proj_08_CupcakeCornerApp.swift
+//  Proj-08-CupcakeCorner
+//
+//  Created by Андрей Бородкин on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Proj_08_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
